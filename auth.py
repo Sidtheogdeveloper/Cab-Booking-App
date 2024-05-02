7a3195d94d8843597f6dcf2fe79a8f7b6ce08355
Siddharth <114420294+Sidtheogdeveloper@users.noreply.github.com>
2024-05-02 09:13:24 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,44 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from email.utils import formataddr
+from pathlib import Path
+import random
+
+def gen_otp():
+    otp=''
+    for i in range(6):
+        val= '1234567890'
+        nums= list(val)
+        otp += random.choice(nums)
+    return otp
+
+
+def mail(email_id, nm,otp):
+    PORT = 587
+    EMAIL_SERVER= "smtp-mail.outlook.com"
+
+    sender_email= "siddharthmadhu2005@outlook.com"
+    passwd_email= "Siddharth1795#"
+
+
+    def send_mail(subject, receiver_mail, name, otp):
+        msg= EmailMessage()
+        msg["Subject"]= subject
+        msg["From"]= formataddr(("Cabs", f"{sender_email}"))
+        msg["To"]= receiver_mail
+
+        msg.set_content(
+           f"""Hi {name},\nThe Generated One Time Password for your login request is \n{otp}
+            \n If it wasn't you then please report back on the same email address\n\nRegards\nSupport Team
+            """
+        )
+        with smtplib.SMTP(EMAIL_SERVER, PORT) as server:
+            server.starttls()
+            server.login(sender_email, passwd_email)
+            server.sendmail(sender_email, receiver_mail, msg.as_string())
+    try:
+        send_mail("Login Request", email_id, nm, otp)
+        return 'Done'
+    except:
+        return "error" 
