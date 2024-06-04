f56dba947e1ee02f5adc87d53f54d388def008f3
Siddharth <114420294+Sidtheogdeveloper@users.noreply.github.com>
2024-06-04 13:22:17 +0530

    Add files via upload

---
+++ chat_server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f'Message: {msg}')
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0')
+
